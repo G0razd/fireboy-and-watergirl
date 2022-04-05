13c7c15645969c178db40d76854434fb5d92011b
ezluci <ezluci1@gmail.com>
2022-04-05 22:59:29 +0300

    websocket

--- index.php
+++
@@ -1,5 +0,0 @@
-<html>
-   <body>
-      <h2>another change</h2>
-   </body>
-</html>
---
+++ js/game.js
@@ -0,0 +1,18 @@
+let canvas = document.querySelector('#game').getContext('2d');
+
+canvas.fillStyle = "#FF0000";
+canvas.font = "50px Comic Sans MS";
+canvas.fillText("sarpele", 50, 300);
+canvas.fillText("!", 50, 350);
+
+// websocket
+const socket = new WebSocket('ws://localhost:8080');
+
+socket.onopen = (event) => {
+   console.log('client: m-am conectat');
+   socket.send('m-am conectat!!');
+}
+
+socket.onmessage = (event) => {
+   console.log(event.data);
+}
