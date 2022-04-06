cf1cbba88d34c809d87fd02a4fe48d76b1c9eba3
ezluci <ezluci1@gmail.com>
2022-04-06 15:40:02 +0300

    ws server

--- game.js
+++ game.js
@@ -6,13 +6,3 @@ canvas.fillText("sarpele", 50, 300);
 canvas.fillText("!", 50, 350);
 
 // WebSocket
-const socket = new WebSocket('ws://firegirl-and-waterboy.herokuapp.com');
-
-socket.onopen = (event) => {
-   console.log('client: m-am conectat');
-   socket.send('m-am conectat!!');
-}
-
-socket.onmessage = (event) => {
-   console.log(`data primit: ${event.data}`);
-}
